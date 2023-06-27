3c4680748b4153061cb112b691e660b0193aeb7e
John Fáwọlé <johnfawole18@gmail.com>
2023-06-27 03:50:14 +0100

    Learned How To Nest Components in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function MyButton() {
+  return(
+    <button>
+      Blockchain Alpha is the Best Web3 Technical Content Marketing Agency
+    </button>
   );
-}
+} 
 
-export default App;
+ export default function MyApp() {
+  return (
+    <div>
+      <h1>I Welcome You to Blockchain Alpha! </h1>
+      <MyButton />
+    </div>
+  )
+ }
